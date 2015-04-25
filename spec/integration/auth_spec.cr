6110d75c36149caa4bac7e760012056e77e5386e
Sergio Gil Pérez de la Manga <sgilperez@gmail.com>
2015-04-25 23:00:09 +0200

    Basic auth integration spec

---
+++ spec/integration/auth_spec.cr
@@ -0,0 +1,16 @@
+require "../spec_helper"
+
+describe "Basic auth" do
+  webmock_it "sends the basic auth data" do
+    WebMock.stub(:get, "http://example.org/auth")
+      .with(headers: {"Authorization" => "Basic #{Base64.strict_encode64("user:secret")}"})
+      .to_return(body: "Hello, World")
+
+    lines = capture_lines do |output|
+      Crul::CLI.run!(["get", "http://example.org/auth", "-a", "user:secret"], output).should be_true
+    end
+
+    lines.first.should eq("HTTP/1.1 200 OK")
+    lines.last.should eq("Hello, World")
+  end
+end
